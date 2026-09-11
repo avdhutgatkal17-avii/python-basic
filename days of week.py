0f7dc44518693202ce6d0336420b81afce5f30c3
Avdhut Gatkal <avdhutgatkal17@gmail.com>
2026-09-11 15:44:15 +0530

    Day 2

---
+++ days of week.py
@@ -0,0 +1,21 @@
+num = int(input("Enter a number between 10 and 70:") )
+if (num == 10):
+ print("Monday")
+elif (num == 20):
+ print ("Tuesday")
+elif (num == 30):
+ print ("wednesday")
+elif (num == 40):
+ print ("Thursday")
+
+elif (num == 50):
+ print ("Friday")
+
+elif (num == 60):
+   print("Saturday")
+
+elif (num == 70):
+ print("Sunday")
+
+else:
+    print(" wrong data")
